0769ab8070ab4752aa911de8e0de54e41aeaf071
Pat <patrick.s.santucci@gmail.com>
2019-07-11 14:00:08 +0100

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      state.friends.push(action.friend);
+      return { ...state };
+    case "REMOVE_FRIEND":
+      const newFriends = state.friends.filter(
+        friend => friend.id !== action.id
+      );
+      return { ...state, friends: newFriends };
+    default:
+      return state;
+  }
 }
